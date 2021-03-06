c1ffb0a7e765968451ac4a917102d77f8cebce7d
Ravern Koh <ravernkoh@gmail.com>
2021-03-06 22:44:23 +0800

    remove redundant files

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,16 +1,12 @@
 import 'ts-polyfill/lib/es2019-array';
 
 import puppeteer, { Browser } from 'puppeteer';
-import dbs from './sources/atm/dbs';
-import ocbc from './sources/atm/ocbc';
 import blackball from './sources/boba/blackball';
 import eachACup from './sources/boba/each-a-cup';
 import koi from './sources/boba/koi';
 import liho from './sources/boba/liho';
-import gongCha from './sources/boba/gong-cha';
 
 import { Boba } from './sources/boba/model';
-import { ATM } from './sources/atm/model';
 
 import * as Sentry from '@sentry/node';
 import { readStore, writeStore } from './output';
@@ -34,21 +30,6 @@ try {
   fs.mkdirSync('traces');
 } catch (e) {}
 
-async function atm(browser: Browser) {
-  const tempFunc = async (
-    bankName: string,
-    workFunc: (browser: Browser) => Promise<ATM[]>
-  ) => {
-    const data = readStore('banks.json');
-    writeStore('banks.json', {
-      ...data,
-      [bankName]: await workFunc(browser),
-    });
-  };
-
-  await Promise.all([tempFunc('DBS', dbs), tempFunc('OCBC', ocbc)]);
-}
-
 async function boba(browser: Browser) {
   async function tempFunc(
     chainName: string,
@@ -66,7 +47,6 @@ async function boba(browser: Browser) {
   await Promise.all([
     tempFunc('BlackBall', blackball),
     tempFunc('Each-A-Cup', eachACup),
-    tempFunc('Gong Cha', gongCha),
     tempFunc('Koi', koi),
     tempFunc('LiHO', liho),
     tempFunc('ChiCha', chicha),
@@ -87,7 +67,6 @@ async function scraper() {
       : undefined,
   });
 
-  await atm(browser);
   await boba(browser);
 
   await browser.close();
