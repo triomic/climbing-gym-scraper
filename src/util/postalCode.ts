28439f512c7be13059396cd59555c3049b37cd69
Ravern Koh <ravernkoh@gmail.com>
2021-03-08 14:06:49 +0800

    remove old location code

--- src/util/postalCode.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Extract postal code from a raw address string.
- * Returns null if none found
- */
-export default function postalCode(rawAddress: string): string | null {
-  const matches = rawAddress.match(/(\d{6})/);
-  return matches ? matches[0] : null;
-}
